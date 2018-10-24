4436a8bbdb4a8d77a04e7fa55a7ee6bf5fb628d4
Ferit Tunçer <ferit@cryptolab.net>
2018-10-24 14:03:08 +0300

    feat(acordion): half-baked acordion view

--- src/Dispute.js
+++ src/Dispute.js
@@ -30,14 +30,39 @@ disputeStatusToString = (code) => {
 
   render(){
     return (
-      <tr>
-        <td>{this.props.id}</td>
-        <td>{this.props.arbitrated.substring(0, 12) + "..."}</td>
-        <td>{this.props.choices}</td>
-        <td>{web3.utils.fromWei(this.props.fee, 'ether')}</td>
-        <td>{this.props.ruling}</td>
-        <td>{this.disputeStatusToString(this.props.status)}</td>
-      </tr>
+      <div className="card">
+        <div className="card-header" id={this.props.id}>
+          <h5 className="mb-0">
+            <button className="btn btn-link" type="button" data-toggle="collapse" data-target={'#collapse' + this.props.id} aria-expanded="true" aria-controls={'collapse' + this.props.id}>
+              <table>
+                <tr>
+                  <td>
+                    {this.props.id}
+                  </td>
+                  <td>
+                    {this.props.arbitrated}
+                  </td>
+                  <td>
+                    {this.props.fee}
+                  </td>
+                  <td>
+                    {this.props.ruling}
+                  </td>
+                  <td>
+                    {this.props.status}
+                  </td>
+                </tr>
+              </table>
+            </button>
+          </h5>
+        </div>
+
+        <div id={'collapse' + this.props.id} className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+          <div className="card-body">
+            Anim pariatur cliche reprehe
+          </div>
+        </div>
+      </div>
     )
   }
 }
