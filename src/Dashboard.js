f71389af1dec5e0cdb6647e1dfcac40ab3264179
Ferit Tunçer <ferit@cryptolab.net>
2018-11-12 20:03:31 +0300

    refactor(dashboard): link to arbitrator

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -128,7 +128,7 @@ class Dashboard extends React.Component {
     return (
       <div>
         <h4>Owner: {web3.eth.accounts[0] == this.state.owner ? "You" : this.state.owner}</h4>
-        <h4>Arbitrator: {arbitratorInstance.options.address} </h4>
+        <h4>Arbitrator: <a href={"https://kovan.etherscan.io/address/" + arbitratorInstance.options.address} target="_blank">{arbitratorInstance.options.address}</a></h4>
         <form onSubmit={(e) => {e.preventDefault();this.setArbitrationCost(this.state.arbitrationCost)}}>
           <label>
             Arbitration Price: <input type="text" value={this.state.arbitrationCost} onChange={(e) => {this.setState({arbitrationCost: e.target.value})}} />
