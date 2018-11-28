8e836c81e0e0d73b28767b790636900d83d46c9b
Ferit Tunçer <ferit@cryptolab.net>
2018-11-28 15:30:16 +0300

    dispute display under construction

---
+++ src/dispute-display.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import {
+  arbitratorInstance,
+  getOwner,
+  getArbitrationCost,
+  getDispute,
+  getDisputeStatus,
+  setArbitrationPrice
+} from './ethereum/centralized-arbitrator'
+
+class DisputeDisplay from React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount(){
+
+  }
+
+  componentDidUpdate(){
+    
+  }
+
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default DisputeDisplay
