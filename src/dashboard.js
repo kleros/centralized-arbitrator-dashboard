f002e6f8cce4cdbdc50d1fc5f959afc9a3720e75
Ferit Tunçer <ferit@cryptolab.net>
2018-11-28 17:27:24 +0300

    cleanup(dashboard): delete unused dispute event logic

--- src/dashboard.js
+++ src/dashboard.js
@@ -7,10 +7,8 @@ import { arbitrableInstanceAt } from './ethereum/arbitrable'
 import {
   arbitratorInstance,
   getOwner,
-  getArbitrationCost,
   getDispute,
   getDisputeStatus,
-  setArbitrationPrice
 } from './ethereum/centralized-arbitrator'
 import web3 from './ethereum/web3'
 import Identicon from './identicon.js'
@@ -82,123 +80,6 @@ class Dashboard extends React.Component {
     console.log(this.state.contractAddresses)
   }
 
-  updateEvidence = async (disputeID, party, evidence) => {
-    const { disputes } = this.state
-
-    const sortedDisputes = disputes.sort(function(a, b) {
-      return a.id - b.id
-    })
-
-    sortedDisputes[disputeID].evidences[party] =
-      sortedDisputes[disputeID].evidences[party] || []
-
-    fetch(evidence).then(response =>
-      response
-        .json()
-        .catch(function() {
-          console.log('error')
-        })
-        .then(data => sortedDisputes[disputeID].evidences[party].push(data))
-    )
-  }
-
-  updateDispute = async (arbitrableAddress, disputeID, metaEvidenceID) => {
-    const { disputes } = this.state
-
-    const sortedDisputes = disputes.sort(function(a, b) {
-      return a.id - b.id
-    })
-
-    arbitrableInstanceAt(arbitrableAddress)
-      .events.MetaEvidence({
-        filter: { _metaEvidenceID: metaEvidenceID },
-        fromBlock: 0,
-        toBlock: 'latest'
-      })
-      .on('data', event => {
-        fetch(event.returnValues._evidence)
-          .then(response =>
-            response
-              .json()
-              .catch(function() {
-                console.log('error')
-              })
-              .then(data => (sortedDisputes[disputeID].metaevidence = data))
-          )
-          .then(() => this.setState({ disputes: sortedDisputes }))
-      })
-  }
-
-  updateRuling = async event => {
-    const { disputes } = this.state
-    disputes[parseInt(event.returnValues._disputeID)].ruling =
-      event.returnValues[3]
-    disputes[event.returnValues._disputeID].status = await getDisputeStatus(
-      event.returnValues._disputeID
-    )
-    this.setState({ disputes: disputes })
-  }
-
-  addDispute = async (disputeID, arbitrableAddress) => {
-    const dispute = await getDispute(
-      arbitratorInstance(this.state.selectedAddress),
-      disputeID
-    )
-    // dispute.key = disputeID
-    dispute.id = disputeID
-    dispute.evidences = {}
-
-    this.setState(state => ({
-      disputes: [...state.disputes, dispute]
-    }))
-
-    await arbitrableInstanceAt(arbitrableAddress)
-      .events.Dispute({
-        filter: {
-          _arbitrator: this.state.selectedAddress,
-          _disputeID: disputeID
-        },
-        fromBlock: 0,
-        toBlock: 'latest'
-      })
-      .on('data', event => {
-        this.updateDispute(
-          arbitrableAddress,
-          event.returnValues._disputeID,
-          event.returnValues._metaEvidenceID
-        )
-      })
-
-    await arbitrableInstanceAt(arbitrableAddress)
-      .events.Evidence({
-        filter: {
-          _arbitrator: this.state.selectedAddress,
-          _disputeID: disputeID
-        },
-        fromBlock: 0,
-        toBlock: 'latest'
-      })
-      .on('data', event => {
-        this.updateEvidence(
-          disputeID,
-          event.returnValues._party,
-          event.returnValues._evidence
-        )
-      })
-
-    await arbitrableInstanceAt(arbitrableAddress)
-      .events.Ruling({
-        filter: {
-          _arbitrator: this.state.selectedAddress,
-          _disputeID: disputeID
-        },
-        fromBlock: 0,
-        toBlock: 'latest'
-      })
-      .on('data', event => {
-        this.updateRuling(event)
-      })
-  }
 
   owner = () => getOwner(arbitratorInstance(this.state.selectedAddress))
 
