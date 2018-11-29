85f5a695bed580134058590f2bdcf5db776095b5
Ferit Tunçer <ferit@cryptolab.net>
2018-11-29 15:05:30 +0300

    fix(navbar): background color

--- src/navbar.js
+++ src/navbar.js
@@ -4,61 +4,73 @@ import React from 'react'
 import Identicon from './identicon.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
 class NavBar extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-
   }
-  componentDidMount(){
+  componentDidMount() {}
 
-  }
+  componentDidUpdate() {}
 
-  componentDidUpdate(){
-
-  }
-
-  render(){
+  render() {
     const { wallet } = this.props
-    return(
+    return (
       <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark">
           <a class="navbar-brand" href="#">
-            <span><img src="kleros-logo.svg" width="30" height="30" alt="" class="d-inline-block align-top"/></span>
+            <span>
+              <img
+                src="kleros-logo.svg"
+                width="30"
+                height="30"
+                alt=""
+                class="d-inline-block align-top"
+              />
+            </span>
             <span>KLEROS</span>
-
           </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Centralized Arbitrator Dashboard<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">
+                  Centralized Arbitrator Dashboard
+                  <span class="sr-only">(current)</span>
+                </a>
               </li>
-
             </ul>
             <div className="mx-2 my-lg-0">
-            <FontAwesomeIcon icon="bell" className="navbar-icon"/>
+              <FontAwesomeIcon icon="bell" className="navbar-icon" />
             </div>
             <div className="mx-2 my-lg-0">
-            <FontAwesomeIcon icon="envelope" className="navbar-icon" />
+              <FontAwesomeIcon icon="envelope" className="navbar-icon" />
             </div>
             <div className="mx-2 my-lg-0">
-            <Identicon
-              bgColor="#4004A3"
-              className="identicon"
-              color="#009AFF"
-              scale={3}
-              seed={wallet}
-              size={10}
-              spotColor="white"
-            />
+              <Identicon
+                bgColor="#4004A3"
+                className="identicon"
+                color="#009AFF"
+                scale={3}
+                seed={wallet}
+                size={10}
+                spotColor="white"
+              />
             </div>
           </div>
         </nav>
-  </div>
-  )}
+      </div>
+    )
+  }
 }
 
-export default NavBar;
+export default NavBar
