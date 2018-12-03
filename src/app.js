763fbf6245d5238243fca6f1a4c8784071b321b4
Ferit Tunçer <ferit@cryptolab.net>
2018-12-03 19:16:38 +0300

    refactor(linter): app.js fixes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import './app.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
 import {
   faBell,
   faCaretDown,
   faEnvelope,
   faGavel
 } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
 import Dashboard from './dashboard'
+import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 library.add(faGavel)
 library.add(faCaretDown)
