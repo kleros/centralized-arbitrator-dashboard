6d42a3fc1cbdaaadf1bafabbf049a1eea6d2125f
Ferit Tunçer <ferit@cryptolab.net>
2018-12-04 16:27:46 +0300

    refactor(index): popper defined but not used

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import 'bootstrap/dist/js/bootstrap.bundle.min'
 import App from './app'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import _$ from 'jquery'
-import _Popper from 'popper.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
