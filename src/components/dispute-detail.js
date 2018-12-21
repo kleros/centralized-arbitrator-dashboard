e66ee1de63956f5196b69dc29e6f74bc7281a89f
Ferit Tunçer <ferit@cryptolab.net>
2018-12-22 00:36:44 +0300

    fix(ruling-options): generalize

--- src/components/dispute-detail.js
+++ src/components/dispute-detail.js
@@ -1,51 +1,75 @@
-import Archon from '@kleros/archon'
-import EvidenceList from './evidence-list'
-import Identicon from './identicon.js'
-import PropTypes from 'prop-types'
-import React from 'react'
-import { giveRuling } from '../ethereum/centralized-arbitrator'
-import web3 from '../ethereum/web3'
+import Archon from "@kleros/archon";
+import EvidenceList from "./evidence-list";
+import Identicon from "./identicon.js";
+import PropTypes from "prop-types";
+import React from "react";
+import { giveRuling } from "../ethereum/centralized-arbitrator";
+import web3 from "../ethereum/web3";
+import _ from "lodash";
 
 class DisputeDetail extends React.Component {
   constructor(props) {
-    super(props)
-    console.log('DISPUTE DETAIL PROPS')
-    console.log(props)
-    this.archon = new Archon(window.web3.currentProvider)
-    console.log(this.archon)
+    super(props);
+    console.log("DISPUTE DETAIL PROPS");
+    console.log(props);
+    this.archon = new Archon(window.web3.currentProvider);
+    console.log(this.archon);
     this.state = {
-      validationResult: 'Integrity not tested.'
-    }
+      validationResult: "Integrity not tested."
+    };
   }
 
   handleGiveRulingButtonClick = (account, instance, id, ruling) => () => {
-    giveRuling(account, instance, id, ruling) /* Why don't we await? */
-  }
+    console.log(ruling);
+    giveRuling(account, instance, id, ruling); /* Why don't we await? */
+  };
 
   validate(fileURI, fileHash) {
-    this.setState({ validationResult: 'Testing integrity...' })
-    console.log('validate')
-    console.log(this.archon)
-    console.log(fileURI)
+    this.setState({ validationResult: "Testing integrity..." });
+    console.log("validate");
+    console.log(this.archon);
+    console.log(fileURI);
     this.archon.utils
       .validateFileFromURI(fileURI, { hash: fileHash, preValidated: true })
       .then(data => {
-        console.log('data')
-        console.log(data) // true
+        console.log("data");
+        console.log(data); // true
         if (data.isValid)
-          this.setState({ validationResult: 'Integrity is intact!' })
-        else this.setState({ validationResult: 'Integrity is broken!' })
+          this.setState({ validationResult: "Integrity is intact!" });
+        else this.setState({ validationResult: "Integrity is broken!" });
       })
       .catch(err => {
-        this.setState({ validationResult: 'Integrity test failed.' })
-        console.log('here')
-        console.error(err)
-      })
+        this.setState({ validationResult: "Integrity test failed." });
+        console.log("here");
+        console.error(err);
+      });
   }
 
+  renderedRulingOptions = (
+    options,
+    activeWallet,
+    centralizedArbitratorInstance,
+    id
+  ) =>
+    _.zip(options.titles, options.descriptions).map((option, key) => {
+      return (
+        <button
+          className="dropdown-item"
+          onClick={this.handleGiveRulingButtonClick(
+            activeWallet,
+            centralizedArbitratorInstance,
+            id,
+            key + 1
+          )}
+        >
+          {option[0]}: {option[1]}
+        </button>
+      );
+    });
+
   render() {
-    console.log('PRERENDER DISPUTEDETAIL')
-    console.log(this.props)
+    console.log("PRERENDER DISPUTEDETAIL");
+    console.log(this.props);
     const {
       activeWallet,
       aliases,
@@ -60,14 +84,14 @@ class DisputeDetail extends React.Component {
       question,
       rulingOptions,
       title
-    } = this.props
+    } = this.props;
 
-    const { validationResult } = this.state
+    const { validationResult } = this.state;
 
-    if (fileURI && validationResult === 'Integrity not tested.')
-      this.validate(ipfsGateway + fileURI, fileURI.split('/')[2])
+    if (fileURI && validationResult === "Integrity not tested.")
+      this.validate(ipfsGateway + fileURI, fileURI.split("/")[2]);
 
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div className="container">
         <div className="row">
@@ -172,34 +196,24 @@ class DisputeDetail extends React.Component {
                     activeWallet,
                     centralizedArbitratorInstance,
                     id,
-                    1
+                    0
                   )}
                 >
-                  {rulingOptions &&
-                    `${rulingOptions.titles[0]}: ${
-                      rulingOptions.descriptions[0]
-                    }`}
+                  {rulingOptions && `Abstain: Refuse to Rule`}
                 </button>
-                <button
-                  className="dropdown-item"
-                  onClick={this.handleGiveRulingButtonClick(
+                {rulingOptions &&
+                  this.renderedRulingOptions(
+                    rulingOptions,
                     activeWallet,
                     centralizedArbitratorInstance,
-                    id,
-                    2
+                    id
                   )}
-                >
-                  {rulingOptions &&
-                    `${rulingOptions.titles[1]}: ${
-                      rulingOptions.descriptions[1]
-                    }`}
-                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -218,6 +232,6 @@ DisputeDetail.propTypes = {
   question: PropTypes.string.isRequired,
   rulingOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
   title: PropTypes.string.isRequired
-}
+};
 
-export default DisputeDetail
+export default DisputeDetail;
