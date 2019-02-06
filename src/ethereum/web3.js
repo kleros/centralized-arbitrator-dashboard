ef701a1aaae3579b18cd51c33454a357f536d45c
Ferit Tunçer <ferit@cryptolab.net>
2019-02-06 15:05:51 +0300

    feat(metamask): ask for permission to read account details

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -3,6 +3,37 @@ import Web3 from 'web3'
 
 let web3
 
+window.addEventListener('load', async () => {
+  // Modern dapp browsers...
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum)
+    try {
+      // Request account access if needed
+      await window.ethereum.enable()
+      // Acccounts now exposed
+      web3.eth.sendTransaction({
+        /* ... */
+      })
+    } catch (error) {
+      // User denied account access...
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider)
+    // Acccounts always exposed
+    web3.eth.sendTransaction({
+      /* ... */
+    })
+  }
+  // Non-dapp browsers...
+  else {
+    console.log(
+      'Non-Ethereum browser detected. You should consider trying MetaMask!'
+    )
+  }
+})
+
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   console.log('Using the web3 object of the window...')
   web3 = new Web3(window.web3.currentProvider)
