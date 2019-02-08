c4365d638d931e2d0aaa5bfc11fc72145a04b962
Ferit Tunçer <ferit@cryptolab.net>
2019-02-08 18:34:49 +0300

    refactor(notification-item): cleanup

--- src/components/notification-item.js
+++ src/components/notification-item.js
@@ -3,42 +3,34 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import TimeAgo from 'react-timeago'
 
-class NotificationItem extends React.Component {
-  render() {
-    const { text, time } = this.props
-
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-1">
-            <label>
-              <FontAwesomeIcon
-                className="primary-inverted"
-                icon="check-circle"
-              />
-            </label>
-          </div>
-          <div className="col-8">
-            <label>{this.props.text}</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <sub className="float-right primary-inverted">
-              <b>
-                <TimeAgo
-                  date={this.props.time}
-                  maxPeriod={3600}
-                  minPeriod={5}
-                />
-              </b>
-            </sub>
-          </div>
-        </div>
-        <hr />
+const NotificationItem = ({ text, time }) => (
+  <div className="container">
+    <div className="row">
+      <div className="col-1">
+        <label>
+          <FontAwesomeIcon className="primary-inverted" icon="check-circle" />
+        </label>
       </div>
-    )
-  }
+      <div className="col-8">
+        <label>{text}</label>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <sub className="float-right primary-inverted">
+          <b>
+            <TimeAgo date={time} maxPeriod={3600} minPeriod={5} />
+          </b>
+        </sub>
+      </div>
+    </div>
+    <hr />
+  </div>
+)
+
+NotificationItem.propTypes = {
+  text: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired
 }
 
 export default NotificationItem
