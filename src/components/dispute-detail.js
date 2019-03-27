f4bd8c26a9a4128dec0ab678a199e62bac0ba1cc
Ferit Tunçer <ferit@cryptolab.net>
2019-03-27 23:18:05 +0300

    feat(dispute-detail): appealable ruling parameter styling

--- src/components/dispute-detail.js
+++ src/components/dispute-detail.js
@@ -190,24 +190,61 @@ class DisputeDetail extends React.Component {
               </div>
             </div>
             {this.state.appealable && (
-              <div className="row border">
-                <div className="col">
-                  <label>Appeal Fee(ETH)</label>{' '}
-                  <input
-                    type="number"
-                    value={this.state.appealFee}
-                    onChange={this.handleAppealFeeChange()}
-                  />
+              <>
+                <div className="row border background-shade mb-5 pt-3">
+                  <div className="col-4 offset-1">
+                    <div className="input-group input-group-sm mb-3">
+                      <div className="input-group-prepend">
+                        <span
+                          className="input-group-text background-shade border-0"
+                          id="inputGroup-sizing-sm"
+                        >
+                          Appeal Fee(ETH)
+                        </span>
+                      </div>
+                      <input
+                        type="number"
+                        className="form-control"
+                        aria-label="Small"
+                        aria-describedby="inputGroup-sizing-sm"
+                        value={this.state.appealFee}
+                        onChange={this.handleAppealFeeChange()}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-2">
+                    <hr
+                      className="mt-0"
+                      style={{
+                        width: '1px',
+                        height: '30px',
+                        background: '#CCCCCC',
+                        border: 'none'
+                      }}
+                    />
+                  </div>
+                  <div className="col-4">
+                    <div className="input-group input-group-sm mb-3">
+                      <div className="input-group-prepend">
+                        <span
+                          className="input-group-text background-shade border-0"
+                          id="inputGroup-sizing-sm"
+                        >
+                          Time to Appeal(Seconds)
+                        </span>
+                      </div>
+                      <input
+                        type="number"
+                        className="form-control"
+                        aria-label="Small"
+                        aria-describedby="inputGroup-sizing-sm"
+                        value={this.state.timeToAppeal}
+                        onChange={this.handleTimeToAppealChange()}
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="col">
-                  <label>Time to Appeal(Seconds)</label>{' '}
-                  <input
-                    type="number"
-                    value={this.state.timeToAppeal}
-                    onChange={this.handleTimeToAppealChange()}
-                  />
-                </div>
-              </div>
+              </>
             )}
             <div className="row">
               <div className="col">
